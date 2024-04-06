041078ccb2615fff39c9099fe26b5dec32487b58
allend4 <g00375372@gmit.ie>
2024-04-06 22:31:01 +0100

    2d-portfolio-kaboom

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. Here is my <a href="https://github.com/allend4" target="_blank">Github</a>!`,
+    "cs-degree": `This is my Software development degree. I hung it on the wall because I'm proud of it. I think it gave me a good foundation.`,
+    "sofa-table": `That's my sofa. I like to relax here.`,
+    tv: `That's my TV.`,
+    resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
+    Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+    library: `There are a lot of programming books on my shelves.
+    I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+    exit: `If you want to exit David's portfolio, just close the tab.`,
+  };
